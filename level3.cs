3e2cd54616b2b77f13bc2db708479bc611e614be
Vladimir2303850 <146060573+Vladimir2303850@users.noreply.github.com>
2023-10-03 10:07:49 +0300

    Add files via upload

---
+++ level3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double a = Math.PI / 5;
+        double b = Math.PI;
+        double h = Math.PI / 25;
+        double sum = 0;
+        double y = 0;
+        double last_number = 0;
+        for (int i = 1; i < 10000; i++)
+        {
+            for (double x = a; x <= b; x += h)
+            {
+                sum += Math.Pow(-1, i) * Math.Cos(i * x) / Math.Pow(i, 2);
+                y = (Math.Pow(x, 2) - Math.Pow(Math.PI, 2) / 2) / 4;
+                Console.WriteLine($"{sum} {y}");
+                last_number = Math.Pow(-1, i) * Math.Cos(i * x) / Math.Pow(i, 2);
+
+            }
+            if (Math.Abs(last_number) < 0.0001)
+            {
+                break;
+            }
+        }
+    }
+}
