41c0110ced67055eca9c187981d07471b8247bec
Vladimir2303850 <146060573+Vladimir2303850@users.noreply.github.com>
2023-10-17 09:24:43 +0300

    Add files via upload

---
+++ level_3_upd.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double a = Math.PI / 5;
+        double b = Math.PI;
+        double h = Math.PI / 25;
+        double sum = 0;
+        double y = 0;
+        double lastNumber = 0;
+        int sign = 1;
+        for (int i = 1; ; i++)
+        {
+            double x = a;
+            while (x <= b)
+            {
+                sum += sign * Math.Cos(i * x) / (i * i);
+                y = (x * x - Math.PI * Math.PI / 2) / 4;
+                Console.WriteLine($"{sum} {y}");
+                lastNumber = sign * Math.Cos(i * x) / (i * i);
+
+                x += h;
+            }
+            if (Math.Abs(lastNumber) < 0.0001)
+            {
+                break;
+            }
+            sign *= -1;
+        }
+    }
+}
